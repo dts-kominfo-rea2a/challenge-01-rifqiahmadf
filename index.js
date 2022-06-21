f0f1f587e0adf53a1aabad7281316f0f082bb8d6
rifqiahmadf <rahmadfauzi600@gmail.com>
2022-06-21 13:09:59 +0700

    Rifqi Ahmad Fauzi (152235865101-383)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const firstUser = {
   gender: "Female",
   age: "17",
   email: "monica@dingdong.com",
-  favoriteColor: new Set(["Yellow", "Pink", "White", "Purple"]),
+  favoriteColor: ["Yellow", "Pink", "White", "Purple"],
   isHavePet: "Yes",
   education: [
     {
@@ -24,26 +24,22 @@ const firstUser = {
       city: "Tangerang",
     },
   ],
-  favoriteRestaurant: new Set([
+  favoriteRestaurant: [
     "Bento",
     "Sushi",
     "Pancake",
     "Eggy",
     "Tempura",
-    "Bento",
-    "Eggy",
     "Padang",
     "Tteok",
-    "Sushi",
-    "Sushi",
-  ]),
+  ],
 };
 const secondUser = {
   name: "Wendy",
   gender: "Male",
   age: "23",
   email: "wendy@dingdong.com",
-  favoriteColor: new Set(["Blue", "Black", "Grey"]),
+  favoriteColor: ["Blue", "Black", "Grey"],
   isHavePet: "No",
   education: [
     {
@@ -66,7 +62,7 @@ const secondUser = {
       city: "Tangerang",
     },
   ],
-  favoriteRestaurant: new Set([
+  favoriteRestaurant: [
     "Tempura",
     "Bento",
     "Sushi",
@@ -74,9 +70,8 @@ const secondUser = {
     "Padang",
     "Katsu",
     "Geprek",
-    "Pancake",
     "Eggy",
-  ]),
+  ],
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
